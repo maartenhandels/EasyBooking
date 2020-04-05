052f86c978e7f502c9fb1275e212e2be4c439782
gabrigaraizabal <gabrigaraizabal@opendeusto.es>
2020-04-05 13:05:50 +0200

    Añadido de pantalla inicio sesion-registro

---
+++ src/main/java/LP/InicioSesion_Registro.java
@@ -0,0 +1,161 @@
+package LP;
+
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class InicioSesion_Registro {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InicioSesion_Registro window = new InicioSesion_Registro();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public InicioSesion_Registro() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.setSize(750, 800);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(95, 158, 160));
+		panel_3.setBounds(15, 318, 336, 410);
+		frame.getContentPane().add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblInicioDeSesion = new JLabel("INICIO DE SESION");
+		lblInicioDeSesion.setForeground(new Color(255, 255, 255));
+		lblInicioDeSesion.setBounds(49, 16, 232, 31);
+		lblInicioDeSesion.setFont(new Font("Century Gothic", Font.BOLD, 25));
+		panel_3.add(lblInicioDeSesion);
+		
+		textField = new JTextField();
+		textField.setBounds(15, 130, 306, 40);
+		panel_3.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setForeground(new Color(255, 255, 255));
+		lblEmail.setFont(new Font("Century Gothic", Font.BOLD, 22));
+		lblEmail.setBounds(15, 83, 88, 31);
+		panel_3.add(lblEmail);
+		
+		JLabel label_1 = new JLabel("Contrasenya");
+		label_1.setForeground(new Color(255, 255, 255));
+		label_1.setFont(new Font("Century Gothic", Font.BOLD, 22));
+		label_1.setBounds(15, 207, 142, 31);
+		panel_3.add(label_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(15, 254, 306, 40);
+		panel_3.add(textField_1);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setFont(new Font("Century Gothic", Font.BOLD, 18));
+		btnEntrar.setBounds(101, 354, 119, 40);
+		panel_3.add(btnEntrar);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(95, 158, 160));
+		panel_1.setBounds(0, 0, 728, 60);
+		frame.getContentPane().add(panel_1);
+		
+		JLabel lblEasyBooking = new JLabel("EASYBOOKING");
+		lblEasyBooking.setForeground(new Color(255, 255, 255));
+		lblEasyBooking.setFont(new Font("Century Gothic", Font.BOLD, 40));
+		panel_1.add(lblEasyBooking);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(95, 158, 160));
+		panel_2.setBounds(377, 318, 336, 410);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		ImageIcon img2 = new ImageIcon("src/main/resources/images/facebook.jpg");
+		JButton btn1 = new JButton(img2);
+		btn1.setBounds(70, 100, 200, 88);
+		panel_2.add(btn1);
+		
+		ImageIcon img3 = new ImageIcon("src/main/resources/images/google.jpg");
+		JButton btn2 = new JButton(img3);
+		btn2.setBounds(70, 200, 200, 100);
+		panel_2.add(btn2);
+		
+		JLabel label = new JLabel("REGISTRO");
+		label.setForeground(new Color(255, 255, 255));
+		label.setBounds(109, 16, 116, 32);
+		label.setFont(new Font("Century Gothic", Font.BOLD, 25));
+		panel_2.add(label);
+		
+		JButton button = new JButton("Registrar");
+		button.setFont(new Font("Century Gothic", Font.BOLD, 18));
+		button.setBounds(109, 354, 119, 40);
+		panel_2.add(button);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(95, 158, 160));
+		panel_4.setBounds(0, 58, 728, 200);
+		panel_4.setLayout(null);
+		frame.getContentPane().add(panel_4);
+		
+		ImageIcon img = new ImageIcon("src/main/resources/images/flight.png");
+		JLabel lblImage = new JLabel(img);
+		lblImage.setBounds(270, 0, 200, 200);
+		panel_4.add(lblImage);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setForeground(new Color(255, 255, 255));
+		panel_5.setBounds(0, 83, 255, 17);
+		panel_4.add(panel_5);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setForeground(new Color(255, 255, 255));
+		panel_6.setBounds(464, 83, 264, 17);
+		panel_4.add(panel_6);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 258, 728, 60);
+		frame.getContentPane().add(panel);
+		
+		JLabel lblDisfrutaDeLa = new JLabel("¡Disfruta de la experiencia completa!");
+		lblDisfrutaDeLa.setFont(new Font("Century Gothic", Font.BOLD, 25));
+		panel.add(lblDisfrutaDeLa);
+	
+	}
+}
