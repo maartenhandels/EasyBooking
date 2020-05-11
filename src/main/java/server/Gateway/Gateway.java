6b022c442b7dc9eb977423d25540517b6a1fdb5f
Laura <laura.llorente@opendeusto.es>
2020-05-11 18:19:12 +0200

    Gateway interfaz

--- src/main/java/server/Gateway/Gateway.java
+++ src/main/java/server/Gateway/Gateway.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import server.DTO.UsuarioDTO;
 import server.DTO.VueloDTO;
+import server.LD.Usuario;
 
 public class Gateway implements itfGateway
 {
@@ -18,6 +19,55 @@ public class Gateway implements itfGateway
 	public List<VueloDTO> getVuelos() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public String make_Payment(String email, float cant_total, String concepto) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String create_User(Usuario us, float divisa) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update_currency(String email, float divisa) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean log_in(String email, String password) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String create_User(Usuario us) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean change_password(String email, String old_password, String new_password) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete_user(String email, String password) 
+	{
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
--- src/main/java/server/Gateway/itfGateway.java
+++ src/main/java/server/Gateway/itfGateway.java
@@ -4,9 +4,26 @@ import java.util.List;
 
 import server.DTO.UsuarioDTO;
 import server.DTO.VueloDTO;
+import server.LD.Usuario;
+import server.LD.Vuelo;
 
 public interface itfGateway 
 {
 	public List <UsuarioDTO> getUsuarios();
 	public List <VueloDTO> getVuelos();
+	
+	//Servicio externo - aerolinea
+	//public List <Vuelo> search_flights();
+	
+	//servicio externo - Pago
+	public String make_Payment (String email, float cant_total, String concepto);
+	public String create_User (Usuario us, float divisa);
+	public boolean update_currency (String email, float divisa);
+	
+	//servicio externo - autenticacion
+	public boolean log_in(String email, String password);
+	public String create_User (Usuario us);
+	public boolean change_password(String email, String old_password, String new_password);
+	public boolean delete_user (String email, String password);
+	
 }
